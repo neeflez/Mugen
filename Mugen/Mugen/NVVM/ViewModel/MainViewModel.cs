d9b4ea925de71b9396c5fbbcc78aecdeb784a7b4
neeflez <nflzpl@gmail.com>
2024-01-19 21:17:45 +0100

    layout changes + button commands

--- Mugen/Mugen/NVVM/ViewModel/MainViewModel.cs
+++ Mugen/Mugen/NVVM/ViewModel/MainViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Mugen.Core;
 using System;
+using System.Diagnostics;
+using System.Windows.Input;
 
 namespace Mugen.NVVM.ViewModel
 {
@@ -22,6 +24,26 @@ namespace Mugen.NVVM.ViewModel
         {
             HomeVm = new HomeViewModel();
             CurrentView = HomeVm;
+        }
+
+        private RelayCommand dodajKafelekCommand;
+
+        public ICommand DodajKafelekCommand
+        {
+            get
+            {
+                if (dodajKafelekCommand == null)
+                {
+                    dodajKafelekCommand = new RelayCommand(DodajKafelek);
+                }
+
+                return dodajKafelekCommand;
+            }
+        }
+
+        private void DodajKafelek(object commandParameter)
+        {
+            Debug.WriteLine("dodaj kafelek");
         }
     }
 }
