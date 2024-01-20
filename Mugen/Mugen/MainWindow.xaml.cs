cb08b610d6062eb964f7beb94bb667cad12e7ba3
neeflez <nflzpl@gmail.com>
2024-01-20 19:07:44 +0100

    deleting and adding text to tiles now works

--- Mugen/Mugen/MainWindow.xaml.cs
+++ Mugen/Mugen/MainWindow.xaml.cs
@@ -40,51 +40,79 @@ namespace Mugen
         }
         public void Add_Tile(object sender, RoutedEventArgs e)
         {
-            if (i < ListOfTiles.Count) { 
-            ListOfTiles[i].tile.Visibility = Visibility.Visible;
-            i++;
+            if (i < ListOfTiles.Count)
+            { 
+                ListOfTiles[i].tile.Visibility = Visibility.Visible;
+                ListOfTiles[i].TaskText.Text = textInputTask.Text;
+                ListOfTiles[i].DescriptionText.Text = textInputDescription.Text;
+                i++;
             }
             else
             {
-                MessageBox.Show("Nie dodawaj sobie obwiązków bo wykitujesz :)");
+                MessageBox.Show("Co za dużo to nie zdrowo :)");
             }
         }
 
         public void CreateListOfTiles()
         {
-            Tile_1 = new Tile("asd", "dsad", 1);
+            Tile_1 = new Tile("asd", "dsad");
             Tile_1.tile = Tile1;
+            Tile_1.TaskText = TaskText1;
+            Tile_1.DescriptionText = DescriptionText1;
 
-            Tile_2 = new Tile("asd", "dsad", 1);
+            Tile_2 = new Tile("asd", "dsad");
             Tile_2.tile = Tile2;
+            Tile_2.TaskText = TaskText2;
+            Tile_2.DescriptionText = DescriptionText2;
 
-            Tile_3 = new Tile("asd", "dsad", 1);
+            Tile_3 = new Tile("asd", "dsad");
             Tile_3.tile = Tile3;
+            Tile_3.TaskText = TaskText3;
+            Tile_3.DescriptionText = DescriptionText3;
 
-            Tile_4 = new Tile("asd", "dsad", 1);
+            Tile_4 = new Tile("asd", "dsad");
             Tile_4.tile = Tile4;
+            Tile_4.TaskText = TaskText4;
+            Tile_4.DescriptionText = DescriptionText4;
 
-            Tile_5 = new Tile("asd", "dsad", 1);
+            Tile_5 = new Tile("asd", "dsad");
             Tile_5.tile = Tile5;
+            Tile_5.TaskText = TaskText5;
+            Tile_5.DescriptionText = DescriptionText5;
 
-            Tile_6 = new Tile("asd", "dsad", 1);
+            Tile_6 = new Tile("asd", "dsad");
             Tile_6.tile = Tile6;
+            Tile_6.TaskText = TaskText6;
+            Tile_6.DescriptionText = DescriptionText6;
 
             ListOfTiles = new List<Tile> { Tile_1, Tile_2, Tile_3, Tile_4, Tile_5, Tile_6 };
+        }
+        public void DeleteTile(object sender, RoutedEventArgs e)
+        {
+            if (sender is FrameworkElement frameworkElement)
+            {
+                var xNameProperty = frameworkElement.GetType().GetProperty("Name");
+                if (xNameProperty != null)
+                {
+                    string xNameValue = xNameProperty.GetValue(frameworkElement) as string;
+                    int j = xNameValue[xNameValue.Length-1] - 49;
+                    ListOfTiles[j].tile.Visibility = Visibility.Collapsed;
+                    i--;
+                }
+            }
         }
     }
     public class Tile
     {
         string task;
         string description;
-        int num;
         public Grid tile;
-        public Tile(string _task, string _description, int _num)
+        public TextBlock TaskText;
+        public TextBlock DescriptionText;
+        public Tile(string _task, string _description)
         {
             this.description = _description;
             this.task = _task;
-            this.num = _num;
-
         }
     }
 
