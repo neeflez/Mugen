c94ee3269fabd874060e14b62829631c455be33b
neeflez <nflzpl@gmail.com>
2024-01-23 21:49:29 +0100

    improved new object declaration

--- Mugen/Mugen/MainWindow.xaml.cs
+++ Mugen/Mugen/MainWindow.xaml.cs
@@ -61,35 +61,17 @@ namespace Mugen
 
         public void CreateListOfTiles()
         {
-            Tile_1 = new Tile("asd", "dsad", false);
-            Tile_1.tile = Tile1;
-            Tile_1.TaskText = TaskText1;
-            Tile_1.DescriptionText = DescriptionText1;
+            Tile_1 = new Tile(Tile1, TaskText1, DescriptionText1, false);
 
-            Tile_2 = new Tile("asd", "dsad", false);
-            Tile_2.tile = Tile2;
-            Tile_2.TaskText = TaskText2;
-            Tile_2.DescriptionText = DescriptionText2;
+            Tile_2 = new Tile(Tile2, TaskText2, DescriptionText2, false);
 
-            Tile_3 = new Tile("asd", "dsad", false);
-            Tile_3.tile = Tile3;
-            Tile_3.TaskText = TaskText3;
-            Tile_3.DescriptionText = DescriptionText3;
+            Tile_3 = new Tile(Tile3, TaskText3, DescriptionText3, false);
 
-            Tile_4 = new Tile("asd", "dsad", false);
-            Tile_4.tile = Tile4;
-            Tile_4.TaskText = TaskText4;
-            Tile_4.DescriptionText = DescriptionText4;
+            Tile_4 = new Tile(Tile4, TaskText4, DescriptionText4, false);
 
-            Tile_5 = new Tile("asd", "dsad", false);
-            Tile_5.tile = Tile5;
-            Tile_5.TaskText = TaskText5;
-            Tile_5.DescriptionText = DescriptionText5;
+            Tile_5 = new Tile(Tile5, TaskText5, DescriptionText5, false);
 
-            Tile_6 = new Tile("asd", "dsad", false);
-            Tile_6.tile = Tile6;
-            Tile_6.TaskText = TaskText6;
-            Tile_6.DescriptionText = DescriptionText6;
+            Tile_6 = new Tile(Tile6, TaskText6, DescriptionText6, false);
 
             ListOfTiles = new List<Tile> { Tile_1, Tile_2, Tile_3, Tile_4, Tile_5, Tile_6 };
         }
@@ -126,19 +108,19 @@ namespace Mugen
     }
     public class Tile
     {
-        string task;
-        string description;
         public bool isUsed;
         public Grid tile;
         public TextBlock TaskText;
         public TextBlock DescriptionText;
-        public Tile(string _task, string _description, bool _isUsed)
+        public Tile(Grid _tile, TextBlock _TaskText, TextBlock _DescriptionText, bool _isUsed)
         {
-            this.description = _description;
-            this.task = _task;
+            this.DescriptionText = _DescriptionText;
+            this.TaskText = _TaskText;
+            this.tile = _tile;
             this.isUsed = _isUsed;
         }
     }
+    
 
 
 }
