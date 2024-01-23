cb0a0450f2e35be00d44c420c23d861c68f840d9
neeflez <nflzpl@gmail.com>
2024-01-24 00:15:32 +0100

    +Deadlines list (i have to fix sorting)

--- Mugen/Mugen/MainWindow.xaml.cs
+++ Mugen/Mugen/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -14,6 +15,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Shell;
 
 namespace Mugen
 {
@@ -32,12 +34,15 @@ namespace Mugen
         int k = 0;
         public bool IsPlaceForTile = true;
         List<Tile> ListOfTiles = new List<Tile>();
+        ObservableCollection<Deadline> ListOfDeadlines = new ObservableCollection<Deadline>();
         int i = 0;
         public MainWindow()
         {
             InitializeComponent();
             CreateListOfTiles();
+            DeadlinesList.ItemsSource = ListOfDeadlines;
         }
+
         public void Add_Tile(object sender, RoutedEventArgs e)
         {
             IsPlaceForTile = true;
@@ -45,9 +50,10 @@ namespace Mugen
             {
                 if (ListOfTiles[l].isUsed == false)
                 {
-                    ListOfTiles[l].tile.Visibility = Visibility.Visible;
                     ListOfTiles[l].TaskText.Text = textInputTask.Text;
                     ListOfTiles[l].DescriptionText.Text = textInputDescription.Text;
+                    ListOfTiles[l].DateCreation = DateTime.Now;
+                    ListOfTiles[l].tile.Visibility = Visibility.Visible;
                     ListOfTiles[l].isUsed = true;
                     IsPlaceForTile = false;
                     break;
@@ -58,23 +64,43 @@ namespace Mugen
                 MessageBox.Show("co za dużo to nie zdrowo :)");
             }
         }
+        public void Sort(object sender, RoutedEventArgs e)
+        {
+            List<Deadline> tempList = new List<Deadline>(ListOfDeadlines);
+            tempList.Sort();
 
+            ListOfDeadlines.Clear();
+            foreach (var deadline in tempList)
+            {
+                ListOfDeadlines.Add(deadline);
+            }
+        }
+        public void Sortlist(object sender, RoutedEventArgs e)
+        {
+            ListOfTiles.Sort();
+        }
+        public void Add_Deadline(object sender, RoutedEventArgs e)
+        {
+            Deadline NewDeadline = new Deadline(DeadlineText, DeadLineDate);
+            ListOfDeadlines.Add(NewDeadline);
+        }
         public void CreateListOfTiles()
         {
-            Tile_1 = new Tile(Tile1, TaskText1, DescriptionText1, false);
+            Tile_1 = new Tile(Tile1, TaskText1, DescriptionText1, false, DateTime.Now);
 
-            Tile_2 = new Tile(Tile2, TaskText2, DescriptionText2, false);
+            Tile_2 = new Tile(Tile2, TaskText2, DescriptionText2, false, DateTime.Now);
 
-            Tile_3 = new Tile(Tile3, TaskText3, DescriptionText3, false);
+            Tile_3 = new Tile(Tile3, TaskText3, DescriptionText3, false, DateTime.Now);
 
-            Tile_4 = new Tile(Tile4, TaskText4, DescriptionText4, false);
+            Tile_4 = new Tile(Tile4, TaskText4, DescriptionText4, false, DateTime.Now);
 
-            Tile_5 = new Tile(Tile5, TaskText5, DescriptionText5, false);
+            Tile_5 = new Tile(Tile5, TaskText5, DescriptionText5, false, DateTime.Now);
 
-            Tile_6 = new Tile(Tile6, TaskText6, DescriptionText6, false);
+            Tile_6 = new Tile(Tile6, TaskText6, DescriptionText6, false, DateTime.Now);
 
             ListOfTiles = new List<Tile> { Tile_1, Tile_2, Tile_3, Tile_4, Tile_5, Tile_6 };
         }
+
         public void DeleteTile(object sender, RoutedEventArgs e)
         {
             if (sender is FrameworkElement frameworkElement)
@@ -101,25 +127,64 @@ namespace Mugen
             }
         }
 
+
         public void Shutdown_app(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
         }
+        public void save_tiles_to_file()
+        {
+            ListOfTiles.Sort();
+        }
     }
-    public class Tile
+
+
+    public class Tile 
     {
+        public DateTime DateCreation;
         public bool isUsed;
         public Grid tile;
         public TextBlock TaskText;
         public TextBlock DescriptionText;
-        public Tile(Grid _tile, TextBlock _TaskText, TextBlock _DescriptionText, bool _isUsed)
+        public Tile(Grid _tile, TextBlock _TaskText, TextBlock _DescriptionText, bool _isUsed, DateTime _dateCreation)
         {
             this.DescriptionText = _DescriptionText;
             this.TaskText = _TaskText;
             this.tile = _tile;
             this.isUsed = _isUsed;
+            this.DateCreation = _dateCreation;
         }
+
+
     }
+    public class Deadline : IComparable<Deadline>
+    {
+        public DatePicker DeadlineTime;
+        public TextBox DeadlineText;
+        public Deadline(TextBox _deadlineText, DatePicker _deadlineTime)
+        {
+            this.DeadlineText = _deadlineText;
+            this.DeadlineTime = _deadlineTime;
+        }
+
+        public override string ToString()
+        {
+            return DeadlineText.Text + "\n" + DeadlineTime.SelectedDate.Value.ToString("dd.MM.yyyy");
+        }
+
+
+        int IComparable<Deadline>.CompareTo(Deadline other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            return DeadlineTime.SelectedDate.Value.CompareTo(other.DeadlineTime.SelectedDate.Value);
+        }
+
+
+    }    
+    
     
 
 
